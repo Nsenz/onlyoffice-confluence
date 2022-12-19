cbf715d37c9abec244e3bd1b02bd77431a4321cc
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2022-12-19 16:13:56 +0300

    fixed: getKeyOfFile

--- src/main/java/onlyoffice/managers/document/DocumentManagerImpl.java
+++ src/main/java/onlyoffice/managers/document/DocumentManagerImpl.java
@@ -85,7 +85,7 @@ public class DocumentManagerImpl implements DocumentManager {
             key = generateRevisionId(hashCode);
         }
 
-        return embedded ? key : key + "_embedded";
+        return embedded ? key + "_embedded" : key;
     }
 
     private String generateRevisionId(String expectedKey) {
